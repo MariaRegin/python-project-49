649a4cc7920093d8bc70e1522f5269ee64f034b6
Maria Zhukova <zhukovamaria9@gmail.com>
2022-11-16 00:44:42 +0300

    update script

--- brain_games/scripts/brain_evenscript.py
+++ brain_games/scripts/brain_evenscript.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 
+from brain_games.games.brain_even import provide_task
+from brain_games.games.brain_even import operation_type
+from brain_games.games.brain_even import operation_result
+from brain_games.games.brain_even import operation
+from brain_games.games.brain_even import res
 from brain_games.engine import welcome_user
 from brain_games.engine import explain_details
 from brain_games.engine import launch_game
 from brain_games.engine import name
-from brain_games.engine import operation
-from brain_games.games.brain_even import provide_task
-from brain_games.games.brain_even import operation_type
-from brain_games.games.brain_even import operation_result
 
 
 def main():
     welcome_user()
-    provide_task()
-    explain_details()
-    launch_game()
-    operation_type()
-    operation_result()
+    explain_details(provide_task())
+    launch_game(operation_type(), operation_result())
 
 if __name__ == '__main__':
     main()
