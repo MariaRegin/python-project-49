e0491029f54a282262514034d25589ed41fda977
Maria Zhukova <zhukovamaria9@gmail.com>
2022-11-17 01:53:26 +0300

    modify script

--- brain_games/scripts/brain_evenscript.py
+++ brain_games/scripts/brain_evenscript.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from brain_games.games.brain_even import provide_task
-from brain_games.games.brain_even import operation_type
+from brain_games.games.brain_even import operation
 from brain_games.games.brain_even import operation_result
 from brain_games.engine import welcome_user
 from brain_games.engine import explain_details
@@ -12,7 +12,7 @@ from brain_games.engine import name
 def main():
     welcome_user()
     explain_details(provide_task())
-    launch_game(operation_type(), operation_result())
+    launch_game(operation, operation_result())
 
 if __name__ == '__main__':
     main()
