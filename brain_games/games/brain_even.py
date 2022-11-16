e79c82ebdbfdd289e63c675563f1e0b89e1f483f
Maria Zhukova <zhukovamaria9@gmail.com>
2022-11-17 01:53:03 +0300

    modify game

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -4,17 +4,14 @@ import random
 import prompt
 
 
+operation = random.randint(0, 1000)
+
 def provide_task():
     return('Answer "yes" if the number is even, otherwise answer "no".')
 
-def operation_type():
-    operation = random.randint(0, 1000)
-
 def operation_result():
-    operation = operation_type()
 
     if operation % 2 == 0:
         return('yes')
-
     if operation % 2 > 0:
         return('no')
