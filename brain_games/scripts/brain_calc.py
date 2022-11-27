51832951a1dedf42668698a3f69fbcf0cf556d98
Maria Zhukova <zhukovamaria9@gmail.com>
2022-11-27 21:38:34 +0300

    create brain-calc script

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+import brain_games.games.brain_calc1 as game
+from brain_games.engine import launch_game
+
+
+def main():
+    launch_game(game)
+
+if __name__ == '__main__':
+    main()
