756c542206eef6f195456f061bb3f815ef6693c6
Maria Zhukova <zhukovamaria9@gmail.com>
2023-01-10 10:18:32 +0300

    create brain-prime logic

---
+++ brain_games/games/brain_prime1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import random
+from math import sqrt
+
+
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def operation_result():
+    number = random.randint(2, 100)
+    i = 2
+    while i <= sqrt(number):
+        if number % i == 0:
+            right_answer = 'no'
+            return number, right_answer
+            break
+        i += 1
+    else:
+        right_answer = 'yes'
+        return number, right_answer
+
+
