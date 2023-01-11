4e8f732545095059d9ffa2c69882dbf1663b1947
Maria Zhukova <zhukovamaria9@gmail.com>
2023-01-11 22:34:24 +0300

    correct brain-gcd syntax

--- brain_games/games/brain_gcd1.py
+++ brain_games/games/brain_gcd1.py
@@ -9,7 +9,7 @@ TASK = 'Find the greatest common divisor of given numbers.'
 def operation_result():
     first_number = random.randint(1,1000)
     second_number = random.randint(1,1000)
-    preliminate_number = f'{first_number}, {second_number}'
+    preliminate_number = f'{first_number} {second_number}'
     number = str(preliminate_number)
     preliminate_answer = math.gcd(first_number, second_number)
     right_answer = str(preliminate_answer)
