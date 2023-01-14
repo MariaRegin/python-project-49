d9aa34c96f204b6399734fd5cb094d86a6dc33eb
Maria Zhukova <zhukovamaria9@gmail.com>
2023-01-14 23:54:27 +0300

    delete blank space

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -8,7 +8,7 @@ TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 def operation_result():
     number = random.randint(0, 1000)
     if number % 2 == 0:
-        right_answer = "yes"  
+        right_answer = "yes"
     else:
         right_answer = 'no'
     return number, right_answer
