c6ea0c2bbadad953dd796a9ecb20ceef623a8803
Maria Zhukova <zhukovamaria9@gmail.com>
2023-01-15 23:34:56 +0300

    correct lint mistake

--- brain_games/games/brain_gcd1.py
+++ brain_games/games/brain_gcd1.py
@@ -7,8 +7,8 @@ TASK = 'Find the greatest common divisor of given numbers.'
 
 
 def operation_result():
-    first_number = random.randint(1,1000)
-    second_number = random.randint(1,1000)
+    first_number = random.randint(1, 1000)
+    second_number = random.randint(1, 1000)
     preliminate_number = f'{first_number} {second_number}'
     number = str(preliminate_number)
     preliminate_answer = math.gcd(first_number, second_number)
