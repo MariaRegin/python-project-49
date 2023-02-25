78bb8c1cb6d82db22df587d1bf00318afb993a56
Maria Zhukova <zhukovamaria9@gmail.com>
2023-02-25 22:52:38 +0300

    change variable name and logic

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -2,10 +2,10 @@
 import random
 
 
-TASK = 'What is the result of the expression?'
+DESCRIPTION = 'What is the result of the expression?'
 
 
-def operation_result():
+def calculate_result():
     random_number1 = random.randint(0, 1000)
     random_number2 = random.randint(0, 1000)
     random_number1 = str(random_number1)
@@ -14,8 +14,8 @@ def operation_result():
     expression2 = random_number1 + ' - ' + random_number2
     expression3 = random_number1 + ' + ' + random_number2
     expressions_list = [expression1, expression2, expression3]
-    number = random.choice(expressions_list)
-    splitted_expression = number.split()
+    task = random.choice(expressions_list)
+    splitted_expression = task.split()
     first_number = int(splitted_expression[0])
     second_number = int(splitted_expression[2])
     sign = splitted_expression[1]
@@ -26,4 +26,4 @@ def operation_result():
     if sign == '-':
         result = first_number - second_number
     right_answer = str(result)
-    return number, right_answer
+    return task, right_answer
