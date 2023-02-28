243683148739fdd8067e150562d60e36b2b171b2
Maria Zhukova <zhukovamaria9@gmail.com>
2023-02-28 23:35:16 +0300

    change game logic

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -8,22 +8,23 @@ DESCRIPTION = 'What is the result of the expression?'
 def calculate_result():
     random_number1 = random.randint(0, 1000)
     random_number2 = random.randint(0, 1000)
-    random_number1 = str(random_number1)
-    random_number2 = str(random_number2)
-    expression1 = random_number1 + ' * ' + random_number2
-    expression2 = random_number1 + ' - ' + random_number2
-    expression3 = random_number1 + ' + ' + random_number2
+    expression1 = f"{random_number1} * {random_number2}"
+    expression2 = f"{random_number1} - {random_number2}"
+    expression3 = f"{random_number1} + {random_number2}"
     expressions_list = [expression1, expression2, expression3]
     task = random.choice(expressions_list)
-    splitted_expression = task.split()
-    first_number = int(splitted_expression[0])
-    second_number = int(splitted_expression[2])
-    sign = splitted_expression[1]
-    if sign == '*':
-        result = first_number * second_number
-    if sign == '+':
-        result = first_number + second_number
-    if sign == '-':
-        result = first_number - second_number
-    right_answer = str(result)
+    splitted_list = task.split()
+    first_number = int(splitted_list[0])
+    sign = splitted_list[1]
+    second_number = int(splitted_list[2])
+    def define_sign():
+        if sign == '*':
+            result = first_number * second_number
+        if sign == '+':
+            result = first_number + second_number
+        if sign == '-':
+            result = first_number - second_number
+        preliminary_answer = str(result)
+        return preliminary_answer
+    right_answer = define_sign()
     return task, right_answer
