4e124b1705f4d71a6fcdfa7fa3ff1245d4dffdc2
Maria Zhukova <zhukovamaria9@gmail.com>
2023-03-16 23:04:56 +0300

    change game logic and function name

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,13 +5,15 @@ import random
 DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def calculate_result():
+def get_result():
     task = random.randint(0, 1000)
-    def is_even():
-        if task % 2 == 0:
-            right_answer = "yes"
-        else:
-            right_answer = 'no'
-        return right_answer
-    right_answer = is_even()
+    if is_even(task):
+        right_answer = 'yes'
+    else:
+        right_answer = 'no'
     return task, right_answer
+
+def is_even(question):
+    if question % 2 == 0:
+        return True
+    return False
