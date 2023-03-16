71326a572949494c6198480f410a2413d158a9d7
Maria Zhukova <zhukovamaria9@gmail.com>
2023-03-16 23:05:36 +0300

    change game logic and function name

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 import random
-from math import sqrt
 
 
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def calculate_result():
+def get_result():
     task = random.randint(2, 100)
-    i = 2
-    while i <= sqrt(task):
-        if task % i == 0:
-            right_answer = 'no'
-            return task, right_answer
-            break
-        i += 1
-    else:
+    if is_prime(task):
         right_answer = 'yes'
-        return task, right_answer
+    else:
+        right_answer = 'no'
+    return task, right_answer
+
+def is_prime(question):
+    for i in range(2, int(question / 2) + 1):
+        if (question % i) == 0:
+            return False
+    return True
