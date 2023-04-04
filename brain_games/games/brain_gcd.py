32902ed9f04335136b9e03c23017a2cac1bc0ef0
Maria Zhukova <zhukovamaria9@gmail.com>
2023-04-04 22:59:23 +0300

    change function name

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -6,7 +6,7 @@ import random
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def get_result():
+def generate_round():
     first_number = random.randint(1, 100)
     second_number = random.randint(1, 100)
     task = f'{first_number} {second_number}'
