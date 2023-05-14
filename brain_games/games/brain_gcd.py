95e894beb1ecd3200dc49c989ad13ce5c4ff4c1e
Maria Zhukova <zhukovamaria9@gmail.com>
2023-05-14 15:11:08 +0300

    change logic and variables names

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -9,7 +9,6 @@ DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 def generate_round():
     first_number = random.randint(1, 100)
     second_number = random.randint(1, 100)
-    task = f'{first_number} {second_number}'
-    preliminate_answer = math.gcd(first_number, second_number)
-    right_answer = str(preliminate_answer)
-    return task, right_answer
+    question = f'{first_number} {second_number}'
+    right_answer = math.gcd(first_number, second_number)
+    return question, str(right_answer)
