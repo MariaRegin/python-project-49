4562ef6fb01928bd8d6418a36d5c6dadd0dfa18d
Maria Zhukova <zhukovamaria9@gmail.com>
2023-05-14 15:11:15 +0300

    change logic and variables names

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -6,16 +6,18 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def generate_round():
-    task = random.randint(2, 100)
-    if is_prime(task):
+    question = random.randint(2, 100)
+    if is_prime(question):
         right_answer = 'yes'
     else:
         right_answer = 'no'
-    return task, right_answer
+    return str(question), right_answer
 
 
-def is_prime(question):
-    for i in range(2, int(question / 2) + 1):
-        if (question % i) == 0:
+def is_prime(number):
+    for i in range(2, int(number / 2) + 1):
+        if (number % i) == 0:
             return False
+    if number < 2:
+        return False
     return True
