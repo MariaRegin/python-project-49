f659acaf443f91934466c18b0f46ab37b35fbed9
Maria Zhukova <zhukovamaria9@gmail.com>
2023-05-14 15:10:34 +0300

    change logic and variables names

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -6,14 +6,14 @@ def launch_game(game):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game.DESCRIPTION)
-    tries = 0
-    while tries < 3:
-        tries += 1
-        task, right_answer = game.generate_round()
-        print(f"Question: {task}")
+    wins = 0
+    while wins < 3:
+        question, right_answer = game.generate_round()
+        print(f"Question: {question}")
         answer = prompt.string('Your answer: ')
         if right_answer == answer:
             print('Correct!')
+            wins += 1
         else:
             print(f"'{answer}' is wrong answer ;(. "
                   f"Correct answer was '{right_answer}'."
